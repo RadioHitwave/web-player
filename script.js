4e3bfc8bad1bf1254d83e9a32fe9fb7342964431
Jan Brodda <github@janbrodda.de>
2019-03-07 21:38:13 +0100

    Implement volume adjust feature

--- script.js
+++ script.js
@@ -1,42 +1,83 @@
+function getMediaAmplifier(mediaElem) {
+	var context = new (window.AudioContext || window.webkitAudioContext),
+		result = {
+			context: context,
+			source: context.createMediaElementSource(mediaElem),
+			gain: context.createGain(),
+			media: mediaElem,
+			amplify: function (ampLevel) {
+				result.gain.gain.value = ampLevel;
+			},
+			getAmpLevel: function () {
+				return result.gain.gain.value;
+			}
+		};
+	result.source.connect(result.gain);
+	result.gain.connect(context.destination);
+	return result;
+}
+
 $(document).ready(function () {
-    textFit($("#play"), {alignHoriz: true, alignVert: true});
+	const $play = $("#play");
+	const $stream = $("#stream");
+	const $title = $("#title");
+	const $cover = $("#cover img");
+	const $volume = $("#volumeSlider");
 
-    var initialLoadingDone = false;
-    var originalDocumentTitle = document.title;
+	let streamAmplifier = null;
 
-    $("#stream").on("canplay", function () {
-        initialLoadingDone = true;
-        $("#stream").trigger("play");
-    });
+	$volume.slider({
+		formatter: function (value) {
+			return 'Current value: ' + value;
+		}
+	});
+	$volume.on("slide", function (slideEvt) {
+		if (streamAmplifier === null)
+			streamAmplifier = getMediaAmplifier($stream[0]);
+		streamAmplifier.amplify(slideEvt.value / 100);
+	});
 
-    function ajax() {
-        $.getJSON("https://streamdata.radiohitwave.com/api/", function (data) {
-            $("#cover img").attr("src", data.cover);
-            $("#title").html(data.title);
-            document.title = originalDocumentTitle + " | " + data.title;
-            textFit($("#title"), {alignHoriz: true, alignVert: true, maxFontSize: 22});
-        });
-    }
+	textFit($play, {alignHoriz: true, alignVert: true});
 
-    $("#stream").on("play pause", function () {
-        if (!initialLoadingDone) {
-            $("#play i").addClass("fa-spin fa-spinner");
-            return;
-        }
-        $("#play i").toggleClass("fa-play fa-pause");
-        $("#play i").removeClass("fa-spin fa-spinner");
-    });
+	var initialLoadingDone = false;
+	var originalDocumentTitle = document.title;
 
+	$stream.on("canplay", function () {
+		initialLoadingDone = true;
+		$("#stream").trigger("play");
+	});
 
-    $("#play").click(function () {
-        $stream = $("#stream")[0];
-        if ($stream.paused) {
-            $stream.play();
-        } else if ($stream.readyState >= 3) {
-            $stream.pause();
-        }
-    });
+	function ajax() {
+		$.getJSON("https://streamdata.radiohitwave.com/api/", function (data) {
+			$cover.attr("src", data.cover);
+			$title.html(data.title);
+			document.title = originalDocumentTitle + " | " + data.title;
+			textFit($title, {alignHoriz: true, alignVert: true, maxFontSize: 22});
+		});
+	}
 
-    ajax();
-    setInterval(ajax, 5000);
+	const playIcon = $("#play i");
+	$stream.on("play pause", function () {
+		if (!initialLoadingDone) {
+			$volume.slider("enable");
+			playIcon.addClass("fa-spin fa-spinner");
+			return;
+		}
+		playIcon.toggleClass("fa-play fa-pause");
+		playIcon.removeClass("fa-spin fa-spinner");
+	});
+
+	$play.click(function () {
+		const nativeStream = $stream[0];
+		if (nativeStream.paused) {
+			nativeStream.play();
+		} else if (nativeStream.readyState >= 3) {
+			nativeStream.pause();
+		}
+	});
+
+	ajax();
+	setInterval(ajax, 5000);
+
+	setTimeout(() => $("body").css("visibility", "visible"), 100);
 });
